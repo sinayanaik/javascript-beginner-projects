aaaa33928a2c90b0b9f018a6351b96d6960043cf
sinaya naik <sinayanaik@gmail.com>
2021-08-19 10:34:18 +0530

    change bg color and display the rgb code

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let index =0;
-
 function changeColors(){
     const a = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
     const b = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
     const c = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-    
+    const colorDiv = document.querySelector('.box');
+    let colorcode = `rgb(${a},${b},${c})`
+    colorDiv.textContent = colorcode;
     let UniqueColor = `rgb(${a},${b},${c})`;
     document.getElementsByTagName("body")[0].style.backgroundColor =UniqueColor;
-    console.log(UniqueColor);
 }
